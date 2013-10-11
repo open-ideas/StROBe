9f5f94b7b36b76a2f3260c1ac3d598d3f3db5e0d
rubae <rubenbaetens@gmail.com>
2013-10-11 20:06:23 +0200

    MCSA class

--- Corpus/stats.py
+++ Corpus/stats.py
@@ -2,11 +2,12 @@
 """
 Created on Wed Oct 09 11:57:02 2013
 
-@author: Ruben
+@author: Ruben Baetens
 """
 
 import numpy as np
 import random
+import ast
 
 def get_probability(rnd, prob, p_type='cum'):
     '''
@@ -21,45 +22,55 @@ def get_probability(rnd, prob, p_type='cum'):
         idx += 1
     return idx
 
-def startstate(daytype, name='OSS'):
-    '''
-    Get the startstate for the first simulation day at 4:00 AM
-    '''
-    probs = [daytype[name]['1'], daytype[name]['2'], daytype[name]['3']]
-    state = get_probability(random.random(), probs)
-    return int(state)
-
-def transition(daytype, state, time, name='OPM_'):
-    '''
-    Get next occupancy state in transition from 'state' ending at 'time'.
-    '''
-    probs = daytype[name+str(state)]
-    newoc =get_probability(random.random(), probs[str(time)])
-    return int(newoc)
-
-def duration(daytype, state, time, name='ODM_'):
-    '''
-    Get the duration of current 'state' started at 'time'.
-    '''
-    probs = daytype[name+str(state)]
-    durat = get_probability(random.random(), probs[str(time)])
-    return durat
-
 class MCSA(object):
     '''
-    Monte Carlo Survival Analysis
+    The MCSA class defines a Monte Carlo Survival Analysis
     '''
     # All object parameters are given in kwargs
-    def __init__(self, **kwargs):
-        # copy kwargs to object parameters 
-        for (key, value) in kwargs.items():
-            setattr(self, key, value)        
+    def __init__(self, cluster, **kwargs):
+        # load the dataset of the cluster into ds
+        filnam = 'Occupancies\\'+cluster+'.py'
+        ds = ast.literal_eval(open(filnam).read())
+        # and add them as class parameters
+        self.OSS = ds['OSS']
+        self.OPM = {1:ds['OPM_1'], 2:ds['OPM_2'], 3:ds['OPM_3']}
+        self.ODM = {1:ds['ODM_1'], 2:ds['ODM_2'], 3:ds['ODM_3']}
+        self.RED = ds['RED']
+
+    def startstate(self):
+        '''
+        Get the startstate for first simulation day at 4:00 AM.
+        '''
+        # we define the startstate based on the given probability
+        probs = [self.OSS['1'], self.OSS['2'], self.OSS['3']]
+        state = get_probability(random.random(), probs)
+        # and retrun the value
+        return int(state)
+    
+    def transition(self, state, timebin):
+        '''
+        Get next occupancy state from current state ending at time.
+        '''
+        # we define the new state based on the given probability
+        probs = self.OPM[state][str(timebin)]
+        newoc = get_probability(random.random(), probs)
+        # and retrun the value
+        return int(newoc)
+    
+    def duration(self, state, timebin):
+        '''
+        Get the duration of current state started at time.
+        '''
+        # we define the new duration based on the given probability
+        probs = self.ODM[state][str(timebin)]
+        durat = get_probability(random.random(), probs)
+        # and retrun the value
+        return durat
 
 
-
-class MCdtMc(object):
+class DTMC(object):
     '''
-    Monte Carlo discrete-time Markov chain
+    The DTMC class defines a Discrete-Time Markov Chain
     '''
     # All object parameters are given in kwargs
     def __init__(self, **kwargs):
