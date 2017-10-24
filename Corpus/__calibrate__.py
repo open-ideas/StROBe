c8d287f51620be255ae5b6a59ef246ad7b2500a0
PC3 <astrid.tijskens@gmail.com>
2017-10-24 20:59:16 +0200

    Updated __calibrate__.py to run automatically

--- Corpus/__calibrate__.py
+++ Corpus/__calibrate__.py
@@ -3,20 +3,21 @@
 Created on Wed Oct 09 15:36:23 2013
 
 @author: Ruben Baetens
+
+Reviewed and automated  Oct 2017, Christina Protopapadaki
 """
 
 import os
-import ast
-import residential
+import json
 import cPickle
+import residential
+from Data.Appliances import set_appliances
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 DIR = os.path.dirname(os.path.realpath(__file__))
-os.chdir(os.path.dirname(DIR)+'\\Data')
-
-#for i in range(5):
-#    test = residential.Household('Test_'+str(i))
-#    test.simulate()
-#    test.pickle()
+cwd=os.path.dirname(DIR)+'\\Data'
+os.chdir(cwd)
 
 appliances = ['AnswerMachine','MusicPlayer','Clock','CordlessPhone','HiFi',
               'Iron','Vacuum','Fax','PC','Printer','TV1','TV2','TV3','DVD',
@@ -25,28 +26,71 @@ appliances = ['AnswerMachine','MusicPlayer','Clock','CordlessPhone','HiFi',
 
 flows = ['shortFlow','mediumFlow','showerFlow','bathFlow']
 
-n = dict()
+N=500 # amount of simulated households for calibration 
+rep=10 # amount of repeated calibration rounds to check convergence
+conv=defaultdict(list) #dictionary to save convergence factors per repetition 
 
-for app in appliances:
-    n.update({app:0})
-    n.update({app+'_n':0})
-
-for flow in flows:
-    n.update({flow:0})
-    n.update({flow+'_n':0})
-
-for i in range(5):
-    print i
-    test = cPickle.load(open('Test_'+str(i)+'.p','rb'))
+for j in range(rep):
+    print 'repetition ' , j
+    ## Simulate N test households
+    if not os.path.exists(cwd + '\\Calibration'):
+        os.makedirs(cwd + '\\Calibration') # make folder to keep test households for validation 
+    for i in range(N):
+        if i in range(0,N,10) : print 'Test house '+str(i) + ' created'
+        test = residential.Household('Test_'+str(i))
+        test.simulate()
+        test.roundUp()
+        os.chdir(cwd + '\\Calibration')
+        test.pickle()
+        os.chdir(cwd)
+    
+    n = dict()
+    # initiate with zeros
     for app in appliances:
-        if app in test.n_receptacles.keys():
-            n.update({app:n[app]+test.n_receptacles[app]})
-            n.update({app+'_n':n[app+'_n']+1})
+        n.update({app:0}) # total cycles
+        n.update({app+'_n':0}) # amount of household that have the appliance
+    
+    for flow in flows:
+        n.update({flow:0})
+        n.update({flow+'_n':0})
+    
+    # fill dictionary with data from calibration simulations
+    for i in range(N):
+        if i in range(0,N,10) : print 'Test house '+str(i) + ' checked'
+        test = cPickle.load(open('Calibration\\Test_'+str(i)+'.p','rb'))
+        for app in appliances:
+            if app in test.n_receptacles.keys(): # if household has the appliance
+                n.update({app:n[app]+test.n_receptacles[app]})
+                n.update({app+'_n':n[app+'_n']+1.})
+    
+    # compare number of cycles with reference: "convergence factors"
+    print 'Reference over new cycles \n----------'
+    for app in appliances:
+        eq = residential.Equipment(**set_appliances[app])
+        if n[app+'_n'] != 0 and n[app] != 0:
+            print app, eq.cycle_n/(n[app]/n[app+'_n']) #average amount of cycles per household
+         #   conv.update({app:eq.cycle_n/(n[app]/n[app+'_n'])})
+            conv[app].append(eq.cycle_n/(n[app]/n[app+'_n']))
 
-dataset = ast.literal_eval(open('Appliances.py').read())
+    #calculate new calibration factors
+    print 'New calibration factors \n-----------'
+    for app in appliances:
+        eq = residential.Equipment(**set_appliances[app])
+        if n[app+'_n'] != 0 and n[app] != 0:
+            print app, (eq.cal*eq.cycle_n/(n[app]/n[app+'_n'])+eq.cal)/2
+            set_appliances[app]['cal'] = (eq.cal*eq.cycle_n/(n[app]/n[app+'_n'])+eq.cal)/2
+     
+    # rewrite Appliances.py with new calibration factors        
+    with open('Appliances.py', 'w') as file:
+         file.write('set_appliances = \\\n' + json.dumps( set_appliances,file, indent=2))
 
-for app in appliances:
-    eq = residential.Equipment(**dataset[app])
-    if n[app+'_n'] != 0 and n[app] != 0:
-        print app, eq.cycle_n/(n[app]/n[app+'_n'])
+#save convergence factors
+with open('Convergence.py', 'w') as file:
+    file.write('conv = \\\n' + json.dumps( conv,file, indent=2))
 
+#plot convergence 
+for k in conv:  
+    plt.plot(range(rep),conv[k]);
+plt.axis();
+plt.legend(conv.keys(), loc='lower right');
+plt.show()
